c1b7886c271dd3908afeb3c628d6c3149e123a0f
Morgan Benton <morgan.benton@gmail.com>
2019-05-30 19:45:49 -0400

    moved home/about to pages

---
+++ src/components/pages/TheAboutPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <h2 class="display-2">
+          About
+        </h2>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'TheAboutPage'
+  }
+</script>
---
+++ src/components/pages/TheHomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <h2 class="display-2">
+          Home
+        </h2>
+        <p>
+          Here is some content.
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'TheHomePage'
+  }
+</script>
